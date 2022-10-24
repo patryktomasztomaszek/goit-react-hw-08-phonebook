5ba373c70d93c57833bcb536b373dce0457123c7
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-10-24 23:13:41 +0200

    Typo fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,7 +25,7 @@ export const App = () => {
     if (contacts.some(contact => contact.name === name)) {
       Notify.failure(`${name} already existst in Contact Book!`);
     } else if (contacts.some(contact => contact.number === number)) {
-      Notify.failure(`${number} number already existst in Contact Book!`);
+      Notify.failure(`${number} number already exists in Contact Book!`);
     } else {
       // If there is no duplicates - add contact
       // Contact construct for new contact data
